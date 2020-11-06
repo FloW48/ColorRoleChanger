724385732c6a7deb1bb526748dc3535882164978
FloW48 <florian.fahy@hotmail.fr>
2020-11-06 22:12:44 +0100

    Add server coupains

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ bot.on('ready', async function(){
     bot.user.setActivity("Every 5 seconds, I change color ;)").catch(console.error);
     let server = await bot.guilds.fetch('210094361428492289')
     let role = await server.roles.fetch('773650289601019924')
-    timer(role);
+    let server2 = await bot.guilds.fetch('626684559345451010')
+    let role2 = await server2.roles.fetch('774379949015564348')
+    timer(role, role2);
 })
 
 bot.login('Nzc0MzU5NjAxODA5MTk1MDgw.X6Wokw.bZjIlCX2Lma5ERkqHiKrsLuJuN4')
     .catch(console.error);
 
-async function timer(role){
+async function timer(role, role2){
     let randomColor = random_hex_color_code();
     await role.setColor(randomColor)
+    await role2.setColor(randomColor)
     setTimeout(function(){
-        timer(role)
+        timer(role, role2)
     }, 5000
     )
 }
