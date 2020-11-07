f6039dc62432dd97048199580d532225deec4cf1
FloW48 <florian.fahy@hotmail.fr>
2020-11-07 16:10:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const bot2 = new Discord.Client();
 
 bot.on('ready', async function(){
     bot.user.setActivity("Every 5 seconds, I change color ;)").catch(console.error);
-    let server = await bot.guilds.fetch('210094361428492289')
-    let role = await server.roles.fetch('773650289601019924')
-    let server2 = await bot.guilds.fetch('626684559345451010')
-    let role2 = await server2.roles.fetch('774379949015564348')
-    timer(role, role2);
+    let server1 = await bot.guilds.fetch('626684559345451010')
+    let role = await server1.roles.fetch('774379949015564348')
+    setTimeout(function(){
+        timer(role);
+    }, 1000)
 })
 
-bot.login('Nzc0MzU5NjAxODA5MTk1MDgw.X6Wokw.bZjIlCX2Lma5ERkqHiKrsLuJuN4')
+bot.login('Nzc0MzU5NjAxODA5MTk1MDgw.X6Wokw.RRE7r4V2TiOBkrIjd9vgGTjK3Lc')
     .catch(console.error);
 
-async function timer(role, role2){
+bot2.on('ready', async function(){
+    bot2.user.setActivity("Every 5 seconds, I change color ;)").catch(console.error);
+    let server2 = await bot2.guilds.fetch('210094361428492289')
+    let role = await server2.roles.fetch('774621567317049355')
+    setTimeout(function(){
+        timer(role);
+    }, 1000)
+})
+
+bot2.login('Nzc0NjUwNTYzNDE3MjEwODkw.X6a3jg.D0Cb86dA8hmJh0jeigZ5kRIbkGQ')
+
+async function timer(role){
     let randomColor = random_hex_color_code();
     await role.setColor(randomColor)
-    await role2.setColor(randomColor)
     setTimeout(function(){
-        timer(role, role2)
-    }, 5000
+        timer(role)
+    }, 10000
     )
 }
 
